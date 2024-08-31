d98eba86f26a5458ff615144a75bc1b27708572a
Ayushi Verma <ayushiverma844@gmail.com>
2024-08-31 15:14:07 +0530

    Update and rename find no is positive or negative.c to find no is positive or negative or 0.c

--- Day1/find no is positive or negative or 0.c
+++ Day1/find no is positive or negative or 0.c
@@ -5,8 +5,10 @@ int main()
     printf("Enter a integer except 0:");
     scanf("%d",&i);
     if(i>0)
-    printf("its a positive no.");
+      printf("its a positive no.");
+    else if(i<0)
+      printf("its a negative no.");
     else
-    printf("its a negative no.");
+      printf("its zero");  
     return 0;
 }
