94d64f8c848cf81f5a0e808025d1bc3512a82b53
Ayushi Verma <ayushiverma844@gmail.com>
2024-08-31 17:22:17 +0000

    container config done

---
+++ Day2/Assignment (using logical operator).c
@@ -0,0 +1,20 @@
+//Addmission to a professional course is given according to the following criteria:  marks in physics >=50 & marks in maths >=60 & marks in chem >=55 &total in all three subject >=220 or total in maths and physics>=130.
+   //Wap to accept marks in p,c,m from the user and find out whether the user is eligible for admission or not.
+
+   #include<stdio.h>
+int main()
+{
+    int p,c,m;
+    printf("Enter marks of  p,c,m;");
+    scanf("%d %d %d",&p,&c,&m);
+    if(p>=50 && c>=55 && m>=60)
+    {
+         if((p+c+m)>=220 || (m+p)>=130)
+                printf("you are eligible");
+         else
+                printf("you are not eligible");
+        }
+       else
+       printf("you are not eligible");
+    return 0;
+} 
