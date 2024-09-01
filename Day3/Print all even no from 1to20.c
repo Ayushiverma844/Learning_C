a3bd7b79dbe33504e679b2345d03a3b0d58108b8
Ayushi Verma <ayushiverma844@gmail.com>
2024-09-01 09:10:37 +0000

    container config done

---
+++ Day3/Print all even no from 1to20.c
@@ -0,0 +1,13 @@
+//Wap to print all even no from 1 to 20.
+
+#include<stdio.h>
+int main()
+{
+ int i=2;
+ while(i<=20)
+ {
+     printf("\n%d",i);
+     i=i+2;
+ }
+ printf("\nDone!");
+}
---
+++ Day3/Print all odd no from 1 to 20.c
@@ -0,0 +1,13 @@
+//Wap to print all odd no from 1 to 20.
+
+#include<stdio.h>
+int main()
+{
+ int i=1;
+ while(i<=20)
+ {
+     printf("\n%d",i);
+     i=i+2;
+ }
+ printf("\nDone!");
+}
