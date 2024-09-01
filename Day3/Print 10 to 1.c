e411ea6627f12e972faf85fe3805b1a7575313b0
Ayushi Verma <ayushiverma844@gmail.com>
2024-09-01 09:02:21 +0000

    container config done

---
+++ Day3/Print 10 to 1.c
@@ -0,0 +1,13 @@
+//Wap to print first 10 natural no from 10 to 1.
+
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    while(i>0)
+    {
+        printf("\n%d",i);
+        i=i-1;
+    }
+ printf("\nDone!");
+}
