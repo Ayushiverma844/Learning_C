84ff14ac9858fec357a6fbe1bcfed0d488403824
Ayushi Verma <ayushiverma844@gmail.com>
2024-09-04 03:37:09 +0000

    container config done

---
+++ Day5/2.also display in subject no of higest mark.c
@@ -0,0 +1,1 @@
+//Wap to accept marks of three students, each having 4 subjects & find out the higest mark obtained by each student,also display the sub no in which the student has scored higest mark.
---
+++ Day5/3.Find out overall topper.c
@@ -0,0 +1,1 @@
+//Wap to accept marks of three students, each having 4 subjects & find out overall topper.c
