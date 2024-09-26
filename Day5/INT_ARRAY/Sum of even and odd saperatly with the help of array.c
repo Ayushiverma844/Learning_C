fcc05219d42167da48111680efb6fc93ec8eba8d
Ayushi Verma <ayushiverma844@gmail.com>
2024-09-26 03:46:18 +0000

    container config done

--- Day5/INT_ARRAY/Sum of even and odd saperatly with the help of array.c
+++ Day5/INT_ARRAY/Sum of even and odd saperatly with the help of array.c
@@ -1,15 +1,16 @@
-//Wap to accept 10 integers from the user & then calculate sum of even no and sum of odd no saperatly.
+//Wap to accept a integers from the user & then calculate sum of even no and sum of odd no from 1 to that no saperatly.
+//Assume user will input only positive no.
 
 #include<stdio.h>
+
 int main()
 {
-    int i,soe=0,sod=0;
-    int arr[10];
-    for(i=0;i<=9;i++)
+    int n,soe=0,sod=0;
+    printf("Enter a positive no: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
     {
-        printf("enter no: ");
-        scanf("%d",&arr[i]);
-      (arr[i]%2==0)?(soe=soe+arr[i]):(sod=sod+arr[i]);
+      (i%2==0)?(soe=soe+i):(sod=sod+i);
     }
     printf("Sum of even no is: %d",soe);
     printf("\nSum of odd no is: %d",sod);
