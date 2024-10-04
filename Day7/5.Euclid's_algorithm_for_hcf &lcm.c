dd93736616ee43c3b8cc149608e1d3335ab6bba8
Ayushi Verma <ayushiverma844@gmail.com>
2024-10-04 14:08:32 +0000

    container config done

--- Day7/5.Euclid's_algorithm_for_hcf &lcm.c
+++ Day7/5.Euclid's_algorithm_for_hcf &lcm.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-int a,b,r;
+int a,b,r,m;
 printf("Enter 2 positive no ");
 scanf("%d %d",&a,&b);
+m = (a*b);
  if(a==0||b==0)
     printf("Error");
  while(a%b){
@@ -12,6 +13,6 @@ scanf("%d %d",&a,&b);
      b=r;
  }
  printf("HCF is=%d",b);
- printf("\nLCM is=%d",(a*b)/b);
+ printf("\nLCM is=%d",m/b); // hcf * lcm = a *b
  return 0;
 }
