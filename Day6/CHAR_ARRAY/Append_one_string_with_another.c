44c13f27542d3981c7426848e0f6710e9842b7e2
Ayushi Verma <ayushiverma844@gmail.com>
2025-04-04 08:16:19 +0000

    container config done

--- Day6/CHAR_ARRAY/Append_one_string_with_another.c
+++ Day6/CHAR_ARRAY/Append_one_string_with_another.c
@@ -1,3 +1,14 @@
+Problem Statement:
+Write a C program that takes two strings as input from the user, concatenates them, and prints the resulting string. The program should use the strcat() function from the <string.h> library to perform string concatenation.
+
+Input:
+enter 2 string  
+Hello World
+
+Output:
+HelloWorld
+
+
 #include <stdio.h>
 #include<string.h>
 int main()
