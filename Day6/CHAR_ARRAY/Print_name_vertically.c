ed7fc8f42fa08544f7adb80d3fb14d5b8b14e56c
Ayushi Verma <ayushiverma844@gmail.com>
2025-04-06 08:01:18 +0000

    container config done

--- Day6/CHAR_ARRAY/Print_name_vertically.c
+++ Day6/CHAR_ARRAY/Print_name_vertically.c
@@ -1,4 +1,21 @@
-//Wap to accept a string from the user and display it vertically.
+// Problem Statement:
+
+// Write a program in C that accepts a string from the user and displays each character of the string on a new line, i.e., vertically. The program should handle input and output as follows:
+
+// Prompt the user to enter a string.
+
+// Display the string vertically, with each character printed on a new line.
+
+// Example:
+
+// Input:
+// Enter your name: John
+
+// Output:
+// J
+// o
+// h
+// n
 
 #include<stdio.h>
 int main()
